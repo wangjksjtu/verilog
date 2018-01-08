56ffc3b9232b4ec44afb8a468a0cd7f6b00134f9
wangjksjtu <wangjksjtu_01@sjtu.edu.cn>
2018-01-08 10:35:10 +0800

    Update Counter.v

--- src/Counter.v
+++ src/Counter.v
@@ -22,24 +22,27 @@ module decimal_counter #(parameter N=4) (output reg [N-1:0] count,
             {inf, sup} <= 2'b0;
         end
         else begin
-            if (~dir) begin
-                if (count < 4'd9) begin
-                    count <= count + 4'd1;
-                    {inf, sup} <= 2'b0;
-                end
-                else begin
-                    count <= 4'd0;
-                    {inf, sup} <= 2'b01;
-                end
-            end
+            if (load) count <= data;
             else begin
-                if (count > 4'd0) begin
-                    count <= count - 4'd1;
-                    {inf, sup} <= 2'b0;
+                if (~dir) begin
+                    if (count < 4'd9) begin
+                        count <= count + 4'd1;
+                        {inf, sup} <= 2'b0;
+                    end
+                    else begin
+                        count <= 4'd0;
+                        {inf, sup} <= 2'b01;
+                    end
                 end
                 else begin
-                    count <= 4'd9;
-                    {inf, sup} <= 2'b10;
+                    if (count > 4'd0) begin
+                        count <= count - 4'd1;
+                        {inf, sup} <= 2'b0;
+                    end
+                    else begin
+                        count <= 4'd9;
+                        {inf, sup} <= 2'b10;
+                    end
                 end
             end
         end
